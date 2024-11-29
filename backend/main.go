72201acf73597444a3941ed122512273041bbcc0
Phoebe Harris <aliceharris@murena.io>
2024-11-29 03:01:26 +0000

    Completed task

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,47 @@
 package main
 
-import "net/http"
+import (
+    "fmt"
+	"encoding/json"
+	"net/http"
+	"sync"
+)
+
+type Todo struct {
+	Title string `json:"title"`
+	Description string `json:"description"`
+}
+
+var todoList []Todo = make([]Todo, 0)
+var todoListMutex sync.Mutex
 
 func main() {
 	// Your code here
+	http.HandleFunc("/", ToDoListHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
 func ToDoListHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	// Your code here
+
+	switch r.Method {
+	case "GET":
+		// TODO: Serialize our todo list and send it back
+		todoListMutex.Lock()
+		json.NewEncoder(w).Encode(todoList)
+		todoListMutex.Unlock()
+	case "POST":
+		var item Todo
+		err := json.NewDecoder(r.Body).Decode(&item)
+		if err != nil || len(item.Title) == 0 {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		todoListMutex.Lock()
+		todoList = append(todoList, item)
+		todoListMutex.Unlock()
+		json.NewEncoder(w).Encode(item)
+	}
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,25 +3,56 @@ import './App.css';
 import Todo, { TodoType } from './Todo';
 
 function App() {
+
   const [todos, setTodos] = useState<TodoType[]>([]);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [error, setError] = useState('');
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (!title) {
+      setError("Title must not be empty")
+      return
+    } else {
+      setError("")
+    }
+
+    const newTodo = {
+      title,
+      description
+    }
+
+    setTitle("")
+    setDescription("")
+
+    setTodos([...todos, newTodo])
+
+    fetch("http://localhost:8080/", {
+      method: "POST",
+      body: JSON.stringify(newTodo),
+    })
+  }
 
   // Initially fetch todo
   useEffect(() => {
     const fetchTodos = async () => {
       try {
-        const todos = await fetch('http://localhost:8080/');
-        if (todos.status !== 200) {
+        const todosRequest = await fetch('http://localhost:8080/');
+        if (todosRequest.status !== 200) {
           console.log('Error fetching data');
           return;
         }
 
-        setTodos(await todos.json());
+        setTodos(await todosRequest.json());
       } catch (e) {
         console.log('Could not connect to server. Ensure it is running. ' + e);
       }
     }
 
     fetchTodos()
+
   }, []);
 
   return (
@@ -41,10 +72,11 @@ function App() {
       </div>
 
       <h2>Add a Todo</h2>
-      <form>
-        <input placeholder="Title" name="title" autoFocus={true} />
-        <input placeholder="Description" name="description" />
-        <button>Add Todo</button>
+      <form onSubmit={handleSubmit}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title" name="title" autoFocus={true} />
+        <input value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description" name="description" />
+        {error && <p className="alert">{error}</p>}
+        <button type="submit">Add Todo</button>
       </form>
     </div>
   );
